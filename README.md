b3b52fe45dca6e18d63b144093b11fe88e27bf8e
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-02-13 15:39:18 -0800

    final commit

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Enhanced Random Name Generator
 
 Designed at 1440 by 900 pixels. This website allows the user to input any name they wish into a list that is saved to local storage. The user can then 
 
-2/13/24 pt 1: Basic website styling is done, and the local storage for the name inputs is set up. I am currently working on the group randomization. 2/13/24 pt 2: The group randomization is done, however this is just when the user inputs a group size. I am going to add the ability for the user to input group amount.
+2/13/24 pt 1: Basic website styling is done, and the local storage for the name inputs is set up. I am currently working on the group randomization. 2/13/24 pt 2: The group randomization is done, however this is just when the user inputs a group size. I am going to add the ability for the user to input group amount. 2/13/24 pt 3: I added everything needed. Done.
--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,8 @@ let nameInputBtn = document.getElementById("nameInputBtn");
 let groupInputBtn = document.getElementById("groupInputBtn");
 let nameInput = document.getElementById("nameInput");
 let groupInput = document.getElementById("groupInput");
+let amountInput = document.getElementById("amountInput");
+let amountInputBtn = document.getElementById("amountInputBtn");
 
 let nameAddition = document.getElementById("nameAddition");
 let groupPlacement = document.getElementById("groupPlacement");
@@ -46,48 +48,46 @@ popNames();
 function randomGroups(numby, isTrue) {
     groupPlacement.innerHTML = "";
 
-    let namesArray = getLocalStorage();
-    let intNumby = Number(numby);
-
     let holderDiv = document.createElement("div");
-    holderDiv.className = "";
+    holderDiv.className = "bg-white";
 
     let textP = document.createElement('p');
     textP.className = "text-xl";
     if (isTrue) {
+        let namesArray = getLocalStorage();
+        let intNumby = Number(numby);
+
         textP.innerText = `You Chose The Group Size Of: ${numby}`;
         holderDiv.appendChild(textP);
 
-        let groupP = document.createElement('p');
-        groupP.className = "text-xl";
+        let alertP = document.createElement('p');
+        alertP.className = "text-lg my-2 mx-5";
 
         // intNumby is the group size, namesArray.length is how many people there are, remainder checks to see if there will be people left, sourt removes the remainder to have a balanced namesArray.length
-        // 11, 2
-        if (intNumby === 1) {
-            let randomNum = Math.floor(Math.random() * namesArray.length);
-            groupP.innerText = namesArray[randomNum];
-            holderDiv.appendChild(groupP);
-        } else {
-            let remainder = namesArray.length % intNumby;
+        let remainder = namesArray.length % intNumby;
+        let sourt = namesArray.length - remainder;
+        let groupsCount = sourt / intNumby;
+        let shadowNamesArray = namesArray;
 
-            let alertP = document.createElement('p');
-            alertP.className = "text-lg my-2 mx-5";
+        if (remainder > 1) {
+            groupsCount++;
+            alertP.innerText = `When making groups, there were ${remainder} names remaining. They were put into an unfilled group.`;
+            holderDiv.appendChild(alertP);
+            remainder = 0;
+        } else if (remainder === 1) {
+            alertP.innerText = `When making groups, there was ${remainder} name remaining. They were put into an existing group.`;
+            holderDiv.appendChild(alertP);
+        }
 
-            let sourt = namesArray.length - remainder;
-            let groupsCount = sourt / intNumby;
-            let shadowNamesArray = namesArray;
+        for (let i = 1; i < groupsCount + 1; i++) {
+            let groupP = document.createElement('p');
+            groupP.className = "text-xl mx-5";
 
-            if (remainder > 1) {
-                groupsCount++;
-                alertP.innerText = `When making groups, there were ${remainder} names remaining. They were put into an unfilled group.`;
-                holderDiv.appendChild(alertP);
-                remainder = 0;
-            } else if (remainder === 1) {
-                alertP.innerText = `When making groups, there was ${remainder} name remaining. They were put into an existing group.`;
-                holderDiv.appendChild(alertP);
-            }
-
-            for (let i = 1; i < groupsCount + 1; i++) {
+            if (intNumby === 1) {
+                let randomNum = Math.floor(Math.random() * namesArray.length);
+                groupP.innerText = namesArray[randomNum];
+                holderDiv.appendChild(groupP);
+            } else {
                 groupP.innerText += `Group ${i}: `;
                 for (let j = 0; j < intNumby; j++) {
                     if (shadowNamesArray.length !== 0) {
@@ -107,12 +107,70 @@ function randomGroups(numby, isTrue) {
 
                     remainder--;
                 };
-                groupP.appendChild(document.createElement('br'));
             };
             holderDiv.appendChild(groupP);
         };
     } else {
         textP.innerText = `You Chose A Group Size That Was Invalid Or Not A Number`;
+        holderDiv.appendChild(textP);
+    };
+    groupPlacement.appendChild(holderDiv);
+};
+
+function randomAmount(numby, isTrue) {
+    groupPlacement.innerHTML = "";
+
+    let holderDiv = document.createElement("div");
+    holderDiv.className = "bg-green-300";
+
+    let textP = document.createElement('p');
+    textP.className = "text-xl";
+    if (isTrue) {
+        let namesArray = getLocalStorage();
+        let intNumby = Number(numby);
+
+        textP.innerText = `You Chose The Group Amount Of: ${numby}`;
+        holderDiv.appendChild(textP);
+
+        let alertP = document.createElement('p');
+        alertP.className = "text-lg my-2 mx-5";
+
+        let remainder = namesArray.length % intNumby;
+        let sourt = namesArray.length - remainder;
+        let nameAmount = sourt / intNumby;
+        let shadowNamesArray = namesArray;
+
+        if (remainder !== 0) {
+            alertP.innerText = `When making groups, there were ${remainder} name(s) remaining. They were put into existing groups.`;
+            holderDiv.appendChild(alertP);
+        }
+
+        for (let i = 1; i < intNumby + 1; i++) {
+            let groupP = document.createElement('p');
+            groupP.className = "mx-5 text-xl";
+            groupP.innerText += `Group ${i}: `;
+            for (let j = 0; j < nameAmount; j++) {
+                if (shadowNamesArray.length !== 0) {
+                    let randomNum = Math.floor(Math.random() * shadowNamesArray.length);
+
+                    groupP.innerText += shadowNamesArray[randomNum] + " ";
+
+                    shadowNamesArray.splice(shadowNamesArray.indexOf(shadowNamesArray[randomNum]), 1);
+                };
+            };
+            if (remainder !== 0) {
+                let randomNum = Math.floor(Math.random() * shadowNamesArray.length);
+
+                groupP.innerText += shadowNamesArray[randomNum] + " ";
+
+                shadowNamesArray.splice(shadowNamesArray.indexOf(shadowNamesArray[randomNum]), 1);
+
+                remainder--;
+            };
+            holderDiv.appendChild(groupP);
+        };
+    } else {
+        textP.innerText = `You Chose A Group Amount That Was Invalid Or Not A Number`;
         holderDiv.appendChild(textP);
     };
     groupPlacement.appendChild(holderDiv);
@@ -137,4 +195,16 @@ groupInputBtn.addEventListener('click', function () {
 
     randomGroups(groupInput.value, isValid);
     groupInput.value = "";
+});
+
+amountInputBtn.addEventListener('click', function () {
+    let isValid = true;
+    let namesArray = getLocalStorage();
+
+    if (isNaN(amountInput.value) || amountInput.value > namesArray.length || amountInput.value === "") {
+        isValid = false;
+    };
+
+    randomAmount(amountInput.value, isValid);
+    amountInput.value = "";
 });
